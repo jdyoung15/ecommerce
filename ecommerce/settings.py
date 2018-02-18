69a9a92bd61f9a00cf6cde9a12017dd402a2a3d5
Jeremy Young <jedayoung@gmail.com>
2018-02-18 09:38:14 -0600

    Added reviews for items

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'items.apps.ItemsConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ reviews/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from items.models import Item
+
+# Create your models here.
+class Review(models.Model):
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField('date published')
+  reviewer_name = models.CharField(max_length=64)
+  rating = models.IntegerField()
+  review_content = models.TextField()
