a21a2c2a6730d701b2e1a638f2e0f0bc6d6c1d81
Jeremy Young <jedayoung@gmail.com>
2018-02-18 08:39:32 -0600

    Finished items index and detail skeleton code

--- items/views.py
+++ items/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-  return HttpResponse("List of items for sale.")
+from .models import Item
+
+class IndexView(generic.ListView):
+  template_name = 'items/index.html'
+  context_object_name = 'item_list'
+
+  def get_queryset(self):
+    return Item.objects.order_by('name')
+
+#def index(request):
+#  latest_item_list = Item.objects.order_by('name')[:5]
+#  context = {
+#    'latest_item_list': latest_item_list,
+#  }
+#  return render(request, 'items/index.html', context)
+
+
+class DetailView(generic.DetailView):
+  model = Item
+  template_name = 'items/detail.html'
+
+#def detail(request, item_id):
+#  item = get_object_or_404(Item, pk=item_id)
+#  return render(request, 'items/detail.html', {'item': item})
