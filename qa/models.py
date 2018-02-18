610e1edac0908f6ac8bb8e4de2eb953d3bf89401
Jeremy Young <jedayoung@gmail.com>
2018-02-18 10:03:26 -0600

    Added QA section for items

---
+++ qa/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from items.models import Item
+
+# Create your models here.
+class Question(models.Model):
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField('date published')
+  question_content = models.CharField(max_length=128)
+  votes = models.IntegerField(default=0)
+
+class Answer(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField('date published')
+  author = models.CharField(max_length=64)
+  answer_content = models.TextField()
+  votes = models.IntegerField(default=0)
