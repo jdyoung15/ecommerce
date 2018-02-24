a1c7063db0ab5107c55ff3bf637a58c16ee915ee
Jeremy Young <jedayoung@gmail.com>
2018-02-24 15:15:18 -0600

    Changed qty form number input to select

--- items/forms.py
+++ items/forms.py
@@ -3,16 +3,16 @@ from django import forms
 from .models import InventoryItem
 
 class QtyForm(forms.Form):
+  
+  MIN_QTY = 0
 
   def __init__(self, *args, **kwargs):
     self.item_id = kwargs.pop('item_id', None)
     super(QtyForm, self).__init__(*args, **kwargs)
-    self.fields['qty'] = forms.IntegerField(min_value=0, max_value=self.find_max_qty())
-
-  #qty = forms.IntegerField(min_value=0)
+    self.fields['qty'] = forms.ChoiceField(self.find_number_choices())
 
   def clean_qty(self):
-    qty = self.cleaned_data['qty']
+    qty = int(self.cleaned_data['qty'])
     max_qty = self.find_max_qty()
     if qty > max_qty:
       raise forms.ValidationError("Qty cannot exceed {}!".format(max_qty))
@@ -22,4 +22,8 @@ class QtyForm(forms.Form):
     inventory_item = InventoryItem.objects.filter(item_id=self.item_id).first()
     return inventory_item.qty
     
-    
+  def find_number_choices(self):
+    start = self.MIN_QTY
+    end = self.find_max_qty()
+    zipped = zip(*[range(start, end + 1)] * 2)
+    return [t for t in zipped]
